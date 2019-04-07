5d3c6f68b2a05f9c754340c92ecd8b0cc5a96a8d
BORIS552 <boris.biswas@gmail.com>
2019-04-07 21:35:26 +0530

    LearnEasy

--- app/src/main/java/com/example/lenovo/diabeto/HomeActivity.java
+++ app/src/main/java/com/example/lenovo/diabeto/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.lenovo.diabeto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -12,48 +15,57 @@ public class HomeActivity extends AppCompatActivity {
 
     private TextView day;
     private TextView date;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Date d = Calendar.getInstance().getTime();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
-        String formatDate = sdf.format(d);
-        Calendar calendar = Calendar.getInstance();
-        int var_day = calendar.get(Calendar.DAY_OF_WEEK);
-        day = (TextView)findViewById(R.id.day);
-        date = (TextView)findViewById(R.id.date);
-        switch (var_day){
-            case Calendar.SUNDAY:
-                day.setText("SUNDAY");
-                date.setText(formatDate);
-                break;
-            case Calendar.MONDAY:
-                day.setText("MONDAY");
-                date.setText(formatDate);
-                break;
-            case Calendar.TUESDAY:
-                day.setText("TUESDAY");
-                date.setText(formatDate);
-                break;
-            case Calendar.WEDNESDAY:
-                day.setText("WEDNESDAY");
-                date.setText(formatDate);
-                break;
-            case Calendar.THURSDAY:
-                day.setText("THURSDAY");
-                date.setText(formatDate);
-                break;
-            case Calendar.FRIDAY:
-                day.setText("FRIDAY");
-                date.setText(formatDate);
-                break;
-            case Calendar.SATURDAY:
-                day.setText("SATURDAY");
-                date.setText(formatDate);
-                break;
-        }
+        logout = (Button)findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+//        Date d = Calendar.getInstance().getTime();
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+//        String formatDate = sdf.format(d);
+//        Calendar calendar = Calendar.getInstance();
+//        int var_day = calendar.get(Calendar.DAY_OF_WEEK);
+//        day = (TextView)findViewById(R.id.day);
+//        date = (TextView)findViewById(R.id.date);
+//        switch (var_day){
+//            case Calendar.SUNDAY:
+//                day.setText("SUNDAY");
+//                date.setText(formatDate);
+//                break;
+//            case Calendar.MONDAY:
+//                day.setText("MONDAY");
+//                date.setText(formatDate);
+//                break;
+//            case Calendar.TUESDAY:
+//                day.setText("TUESDAY");
+//                date.setText(formatDate);
+//                break;
+//            case Calendar.WEDNESDAY:
+//                day.setText("WEDNESDAY");
+//                date.setText(formatDate);
+//                break;
+//            case Calendar.THURSDAY:
+//                day.setText("THURSDAY");
+//                date.setText(formatDate);
+//                break;
+//            case Calendar.FRIDAY:
+//                day.setText("FRIDAY");
+//                date.setText(formatDate);
+//                break;
+//            case Calendar.SATURDAY:
+//                day.setText("SATURDAY");
+//                date.setText(formatDate);
+//                break;
+//        }
 
 
 
